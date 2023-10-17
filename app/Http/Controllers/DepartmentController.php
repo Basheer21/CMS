a0d8e03bcdcb0f7fcde2c3052429fbe9b8ec680a
Basheer Raad <basheerraad5@gmail.com>
2023-10-17 18:16:16 +0300

    test

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Department;
+
+use Session;
+
+class DepartmentController extends Controller
+{
+    public function index()
+    {
+        $departments = Department::all();
+        return view('management.departments.index', compact('departments'));
+    }
+
+    public function create()
+    {
+        return view('management.departments.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'          => ['required'],
+            'director_id'   => ['required']
+        ]);
+
+        Department::create([
+            'user_id'           => 1,
+            'director_id'       => $request->director_id,
+            'name'              => $request->name,
+        ]);
+
+        Session::flash('success-message', 'Department created successfully!');
+        return redirect()->route('departmentsIndex');
+    }
+
+    public function edit($id)
+    {
+        $department = Department::find($id);
+        return view('management.departments.edit', compact('department'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'          => ['required'],
+            'director_id'   => ['required']
+        ]);
+        
+        Department::where('id', $id)->update([
+            'director_id'       => $request->director_id,
+            'name'              => $request->name,
+        ]);
+
+        Session::flash('success-message', 'Department updated successfully!');
+        return redirect()->route('departmentsIndex');
+    }
+
+    public function delete($id)
+    {
+        Department::where('id', $id)->delete();
+        Session::flash('success-message', 'Department deleted successfully!');
+        return redirect()->route('departmentsIndex');
+    }
+}
